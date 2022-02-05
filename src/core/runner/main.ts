fd772d18cce7ef4a2019d77f6715b468725c7f64
TeoDev1611 <malehurtadoreyes@hotmail.com>
2022-02-05 18:20:17 -0500

    feat: add the exit to the not succesfully command

--- src/core/runner/main.ts
+++ src/core/runner/main.ts
@@ -1,9 +1,15 @@
 // Copyright © 2022 Dpm Land. All Rights Reserved.
 
+import { LOGGER } from 'mods/logger.ts';
+
 export async function Run(command: string) {
   const cmd = command.split(' ');
   const run = Deno.run({
     cmd: cmd,
   });
-  await run.status();
+  const { success } = await run.status();
+  if (success == false) {
+    LOGGER.error('Command not runned successfully');
+    Deno.exit(1);
+  }
 }
