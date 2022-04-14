b7b1056a13147f2e94209345f56a0e642eccef07
TeoDev1611 <malehurtadoreyes@hotmail.com>
2022-04-14 18:39:09 -0500

    feat: update the dependencies

--- src/modules/deps.ts
+++ src/modules/deps.ts
@@ -4,17 +4,17 @@
 export { dracoFiles, dracoInfo } from 'https://deno.land/x/draco@0.1.3/mod.ts';
 
 // Export the join util
-export { join } from 'https://deno.land/std@0.132.0/path/mod.ts';
+export { join } from 'https://deno.land/std@0.135.0/path/mod.ts';
 
 // Export the base name util
 export {
   basename,
   dirname,
   extname,
-} from 'https://deno.land/std@0.132.0/path/mod.ts';
+} from 'https://deno.land/std@0.135.0/path/mod.ts';
 
 // Ensure exists file for the folder support!
-export { ensureDir, ensureFile } from 'https://deno.land/std@0.132.0/fs/mod.ts';
+export { ensureDir, ensureFile } from 'https://deno.land/std@0.135.0/fs/mod.ts';
 
 // Tables for the authors
 export { table } from 'https://deno.land/x/minitable@v1.0/mod.ts';
@@ -33,15 +33,15 @@ export { soxa } from 'https://deno.land/x/soxa@1.4/mod.ts';
 export {
   Command,
   CompletionsCommand,
-} from 'https://deno.land/x/cliffy@v0.22.2/command/mod.ts';
+} from 'https://deno.land/x/cliffy@v0.23.0/command/mod.ts';
 
 export {
   DenoLandProvider,
   GithubProvider,
   UpgradeCommand,
-} from 'https://deno.land/x/cliffy@v0.22.2/command/upgrade/mod.ts';
+} from 'https://deno.land/x/cliffy@v0.23.0/command/upgrade/mod.ts';
 // Emoji!
 export * as emoji from 'https://deno.land/x/emoji@0.1.2/mod.ts';
 
 // Colors!
-export * as colors from 'https://deno.land/std@0.132.0/fmt/colors.ts';
+export * as colors from 'https://deno.land/std@0.135.0/fmt/colors.ts';
