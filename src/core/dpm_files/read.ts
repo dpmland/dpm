13e3864001e523fed571193313366dcb5322de4a
TeoDev1611 <malehurtadoreyes@hotmail.com>
2022-08-16 15:42:59 -0500

    fix(read/dpm_files): change the read file function and improve a better error out

--- src/core/dpm_files/read.ts
+++ src/core/dpm_files/read.ts
@@ -1,31 +1,72 @@
 // Copyright © 2022 Dpm Land. All Rights Reserved.
 
-import { BASE_DIRECTORIES } from 'mods/dirs.ts';
+import { BASE_DIRECTORIES, NAME_DIRECTORIES } from 'mods/dirs.ts';
 import { LOGGER } from 'mods/logger.ts';
+import { colors } from 'mods/deps.ts';
 
 export async function ReadDpmFile() {
-  try {
-    return JSON.parse(await Deno.readTextFile(BASE_DIRECTORIES.DPM_FILE));
-  } catch (e) {
-    LOGGER.error(e.message);
+  const file = await Deno.readTextFile(BASE_DIRECTORIES.DPM_FILE).catch((e) => {
+    LOGGER.error(
+      `Error at reading the ${
+        colors.dim(NAME_DIRECTORIES.DPM_FILE)
+      }.\nCaused by:\n${colors.bold(e)}`,
+    );
+  });
+
+  if (typeof file != 'string') {
+    LOGGER.error(
+      `Error at the type for parse the JSON file.\n${
+        colors.bold('Please report this at github')
+      }`,
+    );
     Deno.exit(2);
   }
+
+  return JSON.parse(file);
 }
 
 export async function ReadImportMapFile() {
-  try {
-    return JSON.parse(await Deno.readTextFile(BASE_DIRECTORIES.IMPORT_MAPS));
-  } catch (e) {
-    LOGGER.error(e.message);
+  const file = await Deno.readTextFile(BASE_DIRECTORIES.IMPORT_MAPS).catch(
+    (e) => {
+      LOGGER.error(
+        `Error at reading the ${
+          colors.dim(NAME_DIRECTORIES.IMPORT_MAPS)
+        }.\nCaused by:\n${colors.bold(e)}`,
+      );
+    },
+  );
+
+  if (typeof file != 'string') {
+    LOGGER.error(
+      `Error at the type for parse the JSON file.\n${
+        colors.bold('Please report this at github')
+      }`,
+    );
     Deno.exit(2);
   }
+
+  return JSON.parse(file);
 }
 
 export async function ReadDenoConfigFile() {
-  try {
-    return JSON.parse(await Deno.readTextFile(BASE_DIRECTORIES.DENO_JSON_FILE));
-  } catch (e) {
-    LOGGER.error(e.message);
+  const file = await Deno.readTextFile(BASE_DIRECTORIES.DENO_JSON_FILE).catch(
+    (e) => {
+      LOGGER.error(
+        `Error at reading the ${
+          colors.dim(NAME_DIRECTORIES.DENO_JSON_FILE)
+        }.\nCaused by:\n${colors.bold(e)}`,
+      );
+    },
+  );
+
+  if (typeof file != 'string') {
+    LOGGER.error(
+      `Error at the type for parse the JSON file.\n${
+        colors.bold('Please report this at github')
+      }`,
+    );
     Deno.exit(2);
   }
+
+  return JSON.parse(file);
 }
