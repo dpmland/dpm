bfc82a8c7fa5615d6555632ac9aee1c3f2e6b995
TeoDev1611 <malehurtadoreyes@hotmail.com>
2022-09-02 22:12:14 -0500

    feat: improve the basic generate ts file

--- src/commands/bundler.ts
+++ src/commands/bundler.ts
@@ -1,12 +1,14 @@
 // Copyright © 2022 Dpm Land. All Rights Reserved.
 
 import { Command } from 'mods/deps.ts';
+import { generateTypescriptDep } from 'bundler/generate_ts/main.ts';
 
 export const BundlerCommand = new Command()
   .description(
     `Do you want simplfy files or generate files? Generate with this 🤟`,
   )
   .arguments('[action:string]')
-  .action((_, action) => {
+  .action(async (_, action) => {
     console.log(action);
+    await generateTypescriptDep();
   });
---
+++ src/core/bundler/generate_ts/main.ts
@@ -0,0 +1,17 @@
+// Copyright © 2022 Dpm Land. All Rights Reserved.
+
+import { readImportMapFile } from 'json/reader.ts';
+
+// TODO: Add the dependencies organized in groups by url from imports!
+export async function generateTypescriptDep() {
+  const imports = await readImportMapFile();
+
+  let txt = `// Generated with DPM Please no modify!\n\n`;
+  for (const i of Object.keys(imports.imports)) {
+    txt += `// URL -> ${imports.imports[i]} || Module Name -> ${i}\n`;
+    txt += `export * as ${i.replaceAll(/[^A-Za-z0-9]/g, '')} from "${
+      imports.imports[i]
+    }";\n`;
+  }
+  console.log(txt);
+}
