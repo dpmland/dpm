1dae8504c34972df6b10a5e19c3fdb3a95430982
TeoDev1611 <malehurtadoreyes@hotmail.com>
2022-10-09 21:12:29 -0500

    feat: change the installer to monk

--- install.ts
+++ install.ts
@@ -2,10 +2,10 @@
 
 import * as colors from 'https://deno.land/std@0.158.0/fmt/colors.ts';
 import { join } from 'https://deno.land/std@0.158.0/path/mod.ts';
-import { copy } from 'https://deno.land/std@0.158.0/fs/copy.ts';
 import { dracoFiles, dracoInfo } from 'https://deno.land/x/draco@0.1.3/mod.ts';
 import Ask from 'https://deno.land/x/ask@1.0.6/mod.ts';
-import figlet from 'https://x.nest.land/deno-figlet@0.0.5/mod.js';
+import { Monk } from 'https://deno.land/x/monk@0.1.0/mod.ts';
+
 // Utilities
 // Runner function
 async function Run(command: string) {
@@ -34,8 +34,6 @@ async function Run(command: string) {
   }
 }
 
-// Temp Path
-const TEMP = join(dracoFiles.homeDir()!, '.deno', 'dpm');
 // Bin path for the executable
 const BIN = join(dracoFiles.homeDir()!, '.deno', 'bin');
 
@@ -45,152 +43,45 @@ const ask = new Ask({
   prefix: '>',
 });
 
-// Copy the files to the bin function!
-
-async function MoveBinToMain() {
-  if (dracoInfo.platform() == 'windows') {
-    if (dracoFiles.exists(join(TEMP, 'canary', 'dpm.ts'))) {
-      console.log(colors.cyan('Found the DPM executable!\n'));
-      console.log(colors.cyan('Copying the executable to the BIN path!\n'));
-      await copy(
-        `${join(Deno.cwd(), 'dpm.exe')}`,
-        `${join(BIN, 'dpm.exe')}`,
-        { overwrite: true },
-      );
-      console.log(
-        colors.cyan('Removing the dpm.exe file from the current path!\n'),
-      );
-      await Deno.remove(`${join(Deno.cwd(), 'dpm.exe')}`);
-    } else {
-      console.log(
-        colors.red(
-          'Not found the file compiled! Re run the installer or report the error on github.\n',
-        ),
-      );
-    }
-  } else if (
-    dracoInfo.platform() == 'linux' || dracoInfo.platform() == 'darwin'
-  ) {
-    if (dracoFiles.exists(join(TEMP, 'canary', 'dpm.ts'))) {
-      console.log(colors.cyan('Found the DPM executable!\n'));
-      console.log(colors.cyan('Copying the executable to the BIN path!\n'));
-      await copy(
-        `${join(Deno.cwd(), 'dpm')}`,
-        `${join(BIN, 'dpm')}`,
-        { overwrite: true },
-      );
-      console.log(
-        colors.cyan('Removing the dpm file from the current path!\n'),
-      );
-      await Deno.remove(`${join(Deno.cwd(), 'dpm')}`);
-    } else {
-      console.log(
-        colors.red(
-          'Not found the file compiled! Re run the installer or report the error on github.\n',
-        ),
-      );
-    }
-  } else {
-    console.log(
-      colors.red(
-        'Not found the main TypeScript File! Re run the installer or report the error on github.\n',
-      ),
-    );
-  }
-}
-
-// Seppare by functions the CANARY and the STABLE
-
-export async function CanaryInstallation() {
-  console.log('Installing from canary!\n');
-  if (dracoFiles.exists(join(TEMP, 'canary'))) {
-    await Deno.remove(join(TEMP, 'canary'), { recursive: true });
-    console.log(colors.yellow('Cleaned the temp dpm dir!\n'));
-  }
-  await Run(
-    `git clone -b dev --depth=1 https://github.com/dpmland/dpm ${
-      join(TEMP, 'canary')
-    }`,
-  );
-  console.log();
-  console.log('Compiling the executable for better preformance!\n');
-  await Run(
-    `${Deno.execPath()} compile -A --unstable --import-map ${
-      join(TEMP, 'canary', './import_map.json')
-    } --target ${Deno.build.target} ${join(TEMP, 'canary', 'dpm.ts')}`,
-  );
-  await MoveBinToMain();
-}
-
-export async function StableInstallation() {
-  console.log('Install from stable!\n');
-  if (dracoFiles.exists(join(TEMP, 'stable'))) {
-    await Deno.remove(join(TEMP, 'stable'), { recursive: true });
-    console.log(colors.yellow('Cleaned the temp dpm dir!\n'));
-  }
-  await Run(
-    `git clone -b main --depth=1 https://github.com/dpmland/dpm ${
-      join(TEMP, 'stable')
-    }`,
-  );
-  console.log();
-  console.log('Running the installation with the Deno help!\n');
-  await Run(
-    `${Deno.execPath()} compile -A --unstable --import-map ${
-      join(TEMP, 'stable', './import_map.json')
-    } --target ${Deno.build.target} ${join(TEMP, 'stable', 'dpm.ts')}`,
-  );
-  await MoveBinToMain();
-}
-
-// Add the directly access without the prompt!
+let installation: 'canary' | 'stable' = 'stable';
 
 if (Deno.args[0] == 'canary') {
-  await CanaryInstallation();
-  Deno.exit();
+  installation = 'canary';
 }
 if (Deno.args[0] == 'stable') {
-  await StableInstallation();
-  Deno.exit();
+  installation = 'stable';
 }
 
-/*********************************************/
-/*********************************************/
-/**        START THE INSTALLER WORK          */
-/*********************************************/
-/*********************************************/
-
-console.log(colors.cyan(await figlet('DPM UP')));
-
-const answers = await ask.prompt([
-  {
-    name: 'versionOpt',
-    message: 'Install canary or stable ( Answers: canary | stable )',
+await Monk({
+  versions: {
+    downloadVersion: installation,
+    stable: {
+      url: 'https://github.com/dpmland/dpm',
+      branch: 'main',
+    },
+    canary: {
+      url: 'https://github.com/dpmland/dpm',
+      branch: 'dev',
+    },
   },
-]);
+  files: {
+    appName: 'dpm',
+    stable: {
+      importMapFile: './import_map.json',
+      mainFile: './dpm.ts',
+    },
+    canary: {
+      importMapFile: './import_map.json',
+      mainFile: './dpm.ts',
+    },
+  },
 
-switch (answers.versionOpt) {
-  case 'canary': {
-    await CanaryInstallation();
-    break;
-  }
-
-  case 'stable': {
-    await StableInstallation();
-    break;
-  }
-
-  default: {
-    console.log(
-      colors.red(
-        'Not found this target only 2 options: canary | stable.\nClosing the installer',
-      ),
-    );
-    Deno.exit(2);
-  }
-}
-
-console.log();
+  social: {
+    github: 'https://github.com/dpmland/dpm',
+    discord: 'https://discord.com/invite/Um27YPJKud',
+    errors: 'https://github.com/dpmland/dpm/issues',
+  },
+});
 
 const answers2 = await ask.prompt([
   {
@@ -237,12 +128,6 @@ if (answers2.allTools) {
   }
 }
 
-console.log('🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n');
 console.log(
   `Run ${colors.green('dpm --help')} for usage information\n`,
-);
-console.log(
-  `See ${
-    colors.green('https://discord.gg/Um27YPJKud')
-  } for help and propose new ideas at Discord!\n`,
 );
